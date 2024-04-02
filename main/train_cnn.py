64379d4c7fc2e55c83901b44cb3e75e1c008e843
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-04-01 18:50:51 -0700

    added folder_dir variable

--- main/train_cnn.py
+++ main/train_cnn.py
@@ -79,7 +79,8 @@ def main(board_size):
     )    
 
     # Set the save directory
-    save_dir = "trained_models_cnn_mps" if mps_available else "trained_models_cnn"
+    folder_dir = "6x6_new"
+    save_dir = ("trained_models_cnn_mps" if mps_available else "trained_models_cnn") + folder_dir
 
     os.makedirs(save_dir, exist_ok=True)
 
